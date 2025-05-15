c6f31a934cee3d9f1cbd0b3b5f02b3b80efca052
MuntasirSZN <muntasir.joypurhat@gmail.com>
2025-05-15 13:19:59 +0600

    Fix (Drop if possible)

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,17 +3,9 @@
  * do database operations.
  */
 
-import { upstashCache } from 'drizzle-orm/cache/upstash'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import * as schema from './schema'
-import 'dotenv/config'
 
 export const db = drizzle(process.env.DATABASE_URL!, {
-  cache: upstashCache({
-    url: process.env.UPSTASH_REDIS_REST_URL!,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
-    global: true,
-    config: { ex: 30 * 60 },
-  }),
   schema,
 })
