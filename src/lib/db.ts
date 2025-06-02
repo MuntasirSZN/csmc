bc8d02d17bb98e024b2408e1dcf768671126c82f
MuntasirSZN <muntasir.joypurhat@gmail.com>
2025-06-02 12:59:00 +0600

    latest

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,18 +3,18 @@
  * do database operations.
  */
 
-import { upstashCache } from 'drizzle-orm/cache/upstash'
+// import { upstashCache } from 'drizzle-orm/cache/upstash'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import * as schema from './schema'
 
 export const db = drizzle(process.env.DATABASE_URL!, {
-  cache: upstashCache({
-    url: process.env.UPSTASH_REDIS_REST_URL!,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
-    global: true,
-    config: {
-      ex: 30 * 60,
-    },
-  }),
+//  cache: upstashCache({
+//    url: process.env.UPSTASH_REDIS_REST_URL!,
+//    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+//    global: true,
+//    config: {
+//      ex: 30 * 60,
+//    },
+//  }),
   schema,
 })
