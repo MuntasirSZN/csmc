9ba8138575e0345dd86593bc424803b15d390cf0
Muntasir Mahmud <muntasir.joypurhat@gmail.com>
2025-07-07 14:39:58 +0600

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,18 @@
 import antfu from '@antfu/eslint-config'
 import pluginNext from '@next/eslint-plugin-next'
-import youMightNotNeedAnEffect from 'eslint-plugin-react-you-might-not-need-an-effect'
+import reactYouMightNotNeedAnEffect from 'eslint-plugin-react-you-might-not-need-an-effect'
 
 export default antfu({
+  reactYouMightNotNeedAnEffect.configs.recommended,
   formatters: true,
   react: true,
   plugins: {
     '@next/next': pluginNext,
-    'react-you-might-not-need-an-effect': youMightNotNeedAnEffect,
   },
   rules: {
     ...pluginNext.configs.recommended.rules,
     ...pluginNext.configs['core-web-vitals'].rules,
     'node/prefer-global/process': 'off',
-    'react-you-might-not-need-an-effect/you-might-not-need-an-effect': 'warn',
   },
   ignores: [
     'public/**/*',
